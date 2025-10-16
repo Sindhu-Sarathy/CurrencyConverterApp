3897ecd09850c04553a01dd6afdc372bfa7cb538
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2025-10-16 09:23:55 +0200

    Displayed menu for the currency converter app

---
+++ src/main/java/se/lexicon/CurrencyConverterApp.java
@@ -0,0 +1,22 @@
+package se.lexicon;
+import java.util.Scanner;
+
+public class CurrencyConverterApp {
+      public static void main(String[] args){
+          Scanner scanner=new Scanner(System.in);
+          int choice;
+
+         displayMenu();
+
+      }
+
+      private static void displayMenu(){
+          System.out.println("----Currency Converter----");
+          System.out.println("1. Convert SEK to USD");
+          System.out.println("2. Convert USD to SEK");
+          System.out.println("3. Convert SEK to Euro");
+          System.out.println("4. Convert Euro to SEK");
+          System.out.println("0. Exit");
+          System.out.print("Enter your choice: ");
+      }
+}
